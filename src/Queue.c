89594d34be975e9f1afb44d3b633bda19ac80604
Antoine Megarbane <am692@st-andrews.ac.uk>
2024-03-22 16:51:51 +0000

    Implemented all functions in Queue.c

--- src/Queue.c
+++ src/Queue.c
@@ -20,27 +20,54 @@ Queue *new_Queue(int max_size) {
     this.arr = malloc(sizeof(void)*(max_size + 1));
     this.capacity = max_size;
     this.size = ZERO;
+    this.front = ZERO;
+    this.rear = -ONE;
     return this*;
 }
 
 bool Queue_enq(Queue* this, void* element) {
-    return false;
+    if (Queue_size(this) == (*this).capacity || element == NULL) {
+        return false;
+    }
+    else {
+        (*this).rear = ((*this).rear + 1)%(*this).capacity;
+        (*this).arr[(*this).rear] = element;
+        (*this).size++;
+        return true;
+    }
 }
 
 void* Queue_deq(Queue* this) {
-    return NULL;
+    if (Queue_isEmpty(this)) {
+        return NULL;
+    }
+    else {
+        void* element = (*this).arr[(*this).front];
+        (*this).front = ((*this).front + 1)%(*this).capacity;
+        (*this).size--;
+        return element;
+    }
 }
 
 int Queue_size(Queue* this) {
-    return -1;
+    return (*this).size;
 }
 
 bool Queue_isEmpty(Queue* this) {
-    return false;
+    if ((*this).size == ZERO) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 void Queue_clear(Queue* this) {
+    (*this).size = ZERO;
+    (*this).front = ZERO;
+    (*this).rear = -ONE;
 }
 
 void Queue_destroy(Queue* this) {
+    free((*this).arr);
 }
